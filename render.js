8d6fe51089dbbe331abc319da640778e915cbc22
Flynn Buckingham <dev@flynnbuckingham.com>
2017-06-11 17:16:19 -0400

    Added string polyfill for es5 compatibility

--- render.js
+++ render.js
@@ -9,6 +9,49 @@
 var validColorCodes = [1,2,3,4,5,6,7,8,9]; // define what colors are rendered as valid vs non valid - should match templating regex
 // ToDO: consider pulling above statement into ascii renderer function. (May reduce optimization, but should enhance security and prevention of failure).
 
+if (!String.prototype.repeat) { // Mozilla repeat polyfill
+    String.prototype.repeat = function(count) {
+        if (this == null) {
+            throw new TypeError('can\'t convert ' + this + ' to object');
+        }
+        var str = '' + this;
+        count = +count;
+        if (count != count) {
+            count = 0;
+        }
+        if (count < 0) {
+            throw new RangeError('repeat count must be non-negative');
+        }
+        if (count == Infinity) {
+            throw new RangeError('repeat count must be less than infinity');
+        }
+        count = Math.floor(count);
+        if (str.length == 0 || count == 0) {
+            return '';
+        }
+        // Ensuring count is a 31-bit integer allows us to heavily optimize the
+        // main part. But anyway, most current (August 2014) browsers can't handle
+        // strings 1 << 28 chars or longer, so:
+        if (str.length * count >= 1 << 28) {
+            throw new RangeError('repeat count must not overflow maximum string size');
+        }
+        var rpt = '';
+        for (;;) {
+            if ((count & 1) == 1) {
+                rpt += str;
+            }
+            count >>>= 1;
+            if (count == 0) {
+                break;
+            }
+            str += str;
+        }
+        // Could we try:
+        // return Array(count + 1).join(this);
+        return rpt;
+    }
+}
+
 function asciiRender(dataArray, style){
     if (typeof dataArray === "boolean"){
         // shorthand rule for returning readonly valid color Array for rendering.
@@ -104,7 +147,7 @@ function asciiRender(dataArray, style){
     // just dump the entire HTML string (recommended)
 
     if (numSpans) outputString += '</span>'; // adds closure if span tags are present
-    
+
     return outputString
 }
 
